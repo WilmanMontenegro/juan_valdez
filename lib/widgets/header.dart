bd2b80f841077bde3f29bd668f4ee3b3724eacec
Wilman Montenegro <wilman_montenegro@outlook.com>
2024-04-14 22:31:53 -0500

    Actualización de estilos de texto en la página de inicio y en la página de widgets - v1 terminada

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -8,165 +8,173 @@ import '../internacionalizacion.dart';
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
-    return PreferredSize(
-      preferredSize: preferredSize, 
-      // aquí puedes cambiar la altura a tu gusto
-      child: AppBar(
-        automaticallyImplyLeading: false, // oculta el botón de retroceso
-        flexibleSpace: Stack(
-          children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                height: 100, // puedes cambiar la altura a tu gusto
-                color: Color(0xFFD70021),
+    return SafeArea(
+      child: PreferredSize(
+        preferredSize: preferredSize,
+        child: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          automaticallyImplyLeading: false,
+          flexibleSpace: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                  height: 100,
+                  color: Color(0xFFD70021),
+                ),
               ),
-            ),
-            Positioned(
-              top: 0,
-              left: 0,
-              child: Container(
-                // contenedor de la imagen del logo
-                width: MediaQuery.of(context).size.width * 0.22,
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(
-                          0.5), // sombra de color negro con 50% de opacidad
-                      spreadRadius:
-                          1, // se expande 1px en todas las direcciones
-                      blurRadius: 5, // difumina la sombra en un radio de 5px
-                      offset: Offset(1, 1), // changes position of shadow
+              Positioned(
+                top: 0,
+                left: 0,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.22,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 5,
+                        offset: Offset(1, 1),
+                      ),
+                    ],
+                  ),
+                  child: Image.asset(
+                    'assets/images/logo.jpg',
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 23,
+                right: 0,
+                child: Row(
+                  children: <Widget>[
+                    IconButton(
+                      icon: FaIcon(
+                        FontAwesomeIcons.cartShopping,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        // Acción al presionar el icono
+                      },
+                    ),
+                    IconButton(
+                      icon: FaIcon(
+                        FontAwesomeIcons.magnifyingGlass,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        // Acción al presionar el icono
+                      },
+                    ),
+                    IconButton(
+                      icon: Row(
+                        children: <Widget>[
+                          FaIcon(
+                            FontAwesomeIcons.earthAmericas,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 10.0),
+                          Container(
+                            width: 60.0,
+                            child: Text(
+                              'JV en el mundo',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        // Acción al presionar el icono
+                      },
+                    ),
+                    IconButton(
+                      icon: Container(
+                        padding: EdgeInsets.all(2.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(4.0),
+                        ),
+                        child: FaIcon(
+                          FontAwesomeIcons.bars,
+                          color: Color(0xFFD70021),
+                        ),
+                      ),
+                      onPressed: () {
+                        showMenu(
+                          context: context,
+                          position:
+                              RelativeRect.fromLTRB(MediaQuery.of(context).size.width, 140.0, 0.0, 0.0),
+                          items: <PopupMenuEntry>[
+                            PopupMenuItem(
+                              child: Text('Inicio'),
+                              value: 1,
+                            ),
+                            PopupMenuItem(
+                              child: Text('Quienes Somos'),
+                              value: 2,
+                            ),
+                            PopupMenuItem(
+                              child: Text('Medio Ambiente'),
+                              value: 3,
+                            ),
+                            PopupMenuItem(
+                              child: Text('Internacionalización'),
+                              value: 4,
+                            ),
+                          ],
+                          elevation: 8.0,
+                        ).then((value) {
+                          if (value != null) {
+                            switch (value) {
+                              case 1:
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => HomePage()),
+                                );
+                                break;
+                              case 2:
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => QuieneSomos()),
+                                );
+                                break;
+                              case 3:
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => CambioClimatico()),
+                                );
+                                break;
+                              case 4:
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          Internacionalizacion()),
+                                );
+                                break;
+                            }
+                          }
+                        });
+                      },
                     ),
                   ],
                 ),
-                child: Image.asset(
-                  'assets/images/logo.jpg', // reemplaza esto con la ruta de tu imagen
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-        actions: <Widget>[
-          IconButton(
-            icon: FaIcon(
-              FontAwesomeIcons.cartShopping,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              // Acción al presionar el icono
-            },
-          ),
-          IconButton(
-            icon: FaIcon(
-              FontAwesomeIcons.magnifyingGlass,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              // Acción al presionar el icono
-            },
-          ),
-          IconButton(
-            icon: Row(
-              children: <Widget>[
-                FaIcon(
-                  FontAwesomeIcons.earthAmericas,
-                  color: Colors.white,
-                ),
-                SizedBox(width: 10.0), // Espacio entre el icono y el texto
-                Container(
-                  width: 60.0, // Ancho del texto
-                  child: Text(
-                    'JV en el mundo',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
-            ),
-            onPressed: () {
-              // Acción al presionar el icono
-            },
-          ),
-          IconButton(
-            icon: Container(
-              padding: EdgeInsets.all(2.0), // Espacio entre el icono y el borde
-              decoration: BoxDecoration(
-                color: Colors.white, // Relleno blanco
-                border: Border.all(
-                    color: Colors.white,
-                    width: 2.0), // Borde blanco y tamaño del borde
-                borderRadius: BorderRadius.circular(4.0),
-              ),
-              child: FaIcon(
-                FontAwesomeIcons.bars,
-                color: Color(0xFFD70021), // Color del icono
-              ),
-            ),
-            onPressed: () {
-              showMenu(
-                context: context,
-                position: RelativeRect.fromLTRB(
-                    100.0, 100.0, 0.0, 0.0), // puedes cambiar la posición aquí
-                items: <PopupMenuEntry>[
-                  PopupMenuItem(
-                    child: Text('Inicio'),
-                    value: 1,
-                  ),
-                  PopupMenuItem(
-                    child: Text('Quienes Somos'),
-                    value: 2,
-                  ),
-                   PopupMenuItem(
-                    child: Text('Medio Ambiente'),
-                    value: 3,
-                  ),
-                   PopupMenuItem(
-                    child: Text('Internacionalización'),
-                    value: 4,
-                  ),
-
-                  // puedes agregar más opciones aquí
-                ],
-                elevation: 8.0,
-              ).then((value) {
-                if (value != null) {
-                  // manejar la selección del menú aquí
-                  switch (value) {
-                    case 1:
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => HomePage()),
-                      );
-                      break;
-                    case 2:
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => QuieneSomos()),
-                      );
-                      break;
-                      case 3:
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => CambioClimatico()),
-                      );
-                      break;
-                      case 4:
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Internacionalizacion()),
-                      );
-                      break;
-                    // puedes agregar más casos aquí para otras opciones
-                  }
-                }
-              });
-            },
-          ),
-        ],
       ),
     );
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(100.0); // altura del appbar
+  Size get preferredSize => Size.fromHeight(135.0);
 }
